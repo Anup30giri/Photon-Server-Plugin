c97711541045e1298e4983cfd448addb86d259fe
Anup Giri <Anup30giri@hotmail.com>
2023-08-09 20:46:48 +0545

    PhotonPlugin:Communication between Client and Server Configured

---
+++ MyFirstPlugin.cs
@@ -0,0 +1,71 @@
+﻿using Photon.Hive.Plugin;
+using System.Collections.Generic;
+
+namespace MyFirstPlugin
+{
+    public class MyFirstPlugin : PluginBase
+    {
+        public override string Name => "MyFirstPlugin";
+
+        private IPluginLogger pluginLogger;
+
+        public override bool SetupInstance(IPluginHost host, Dictionary<string, string> config, out string errorMsg)
+        {
+            this.pluginLogger = host.CreateLogger(this.Name);
+            return base.SetupInstance(host, config, out errorMsg);
+        }
+
+        public override void OnCreateGame(ICreateGameCallInfo info)
+        {
+            string eventData = "Hello from Photon Server ";
+            this.pluginLogger.InfoFormat($"OnCreateGame {eventData} by user {1} Anup", eventData);
+            //SendCustomEvent(5, eventData);
+            info.Continue();
+        }
+
+        // Method for Sending Data to Client
+        public void RaiseEvent(
+        byte eventCode, 
+        object eventData,
+        byte receiverGroup = ReciverGroup.All,
+        int senderActorNumber = 0,
+        byte cachingOption = CacheOperations.DoNotCache,
+        byte interestGroup = 0,
+        SendParameters sendParams = default(SendParameters))
+        {
+            Dictionary<byte, object> parameters = new Dictionary<byte, object>();
+            parameters.Add(245, eventData);
+            parameters.Add(254, senderActorNumber);
+            PluginHost.BroadcastEvent(receiverGroup, senderActorNumber, interestGroup, eventCode, parameters, cachingOption, sendParams);
+        }
+
+        // On Raise Event
+        public override void OnRaiseEvent(IRaiseEventCallInfo info)
+        {
+            base.OnRaiseEvent(info);
+            this.pluginLogger.InfoFormat("onRaise Event Triggered");
+
+            // Receiving Data from Client
+            byte eventCode = info.Request.EvCode;
+            if (eventCode == 13)
+            {
+                this.pluginLogger.Debug($"Data from event:{eventCode} with information {info.Request.Data}");
+            }
+
+            RaiseEvent(eventCode: 123, eventData: "Hello From Server");
+
+            Dictionary<byte, object> row = new Dictionary<byte, object>() { { 55, "My Testing" }, { 254, 0 } };
+            if (info.IsProcessed)
+            {
+                this.PluginHost.BroadcastEvent(
+                recieverActors: new int[] { 1 },
+                senderActor: 0,
+                data: new Dictionary<byte, object>() { { 245, row } },
+                evCode: 57,
+                cacheOp: 0,
+                sendParameters: new SendParameters() { Unreliable = false });
+            }
+        }
+
+    }
+}
